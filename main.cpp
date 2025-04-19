ec384956cc66e5df6cc47d1b072d578d2a61ff12
Bastian Bernal <bbernal2022@udec.cl>
2025-04-18 22:11:13 -0400

    Feat: Se agrega simulación al main, considerando todos los casos posibles dentro del enunciado.

--- main.cpp
+++ main.cpp
@@ -65,13 +65,13 @@ class Esbirro: public Entidad{
             this->set_ataque(ataque);
             this->cano = is_cano;
         };
-        bool is_cano(){return cano;}
+        bool get_cano(){return cano;}
     private:
         bool cano;
 };
 
 
-void get_input(string* nombre_archivo, deque<Esbirro>* deq_esbirros){
+void get_input(string* nombre_archivo, Entidad* mechon, deque<Esbirro>* deq_esbirros){
 
     vector<int> data = read_file(*nombre_archivo);
 
@@ -83,6 +83,10 @@ void get_input(string* nombre_archivo, deque<Esbirro>* deq_esbirros){
     // Leer valores de vida del héroe y cantidad de esbirros
     int vida_heroe = data[0];
     int cant_esbirros = data[1];
+
+    // Inicializar el héroe
+    mechon->set_vida(vida_heroe);
+    mechon->set_ataque(2);
 
     // Leer valores de esbirros
     for (int i = 0; i < cant_esbirros; i++){
@@ -101,24 +105,64 @@ void get_input(string* nombre_archivo, deque<Esbirro>* deq_esbirros){
         Esbirro esb(vida, ataque, cano);
         deq_esbirros->push_back(esb);
     }
-
-    // Imprimir los valores leídos (para verificar)
-    cout << "Vida del héroe: " << vida_heroe << endl;
-    cout << "Cantidad de esbirros: " << cant_esbirros << endl;
-    cout << "Esbirros:" << endl;
-    for (int i = 0; i < cant_esbirros; i++){
-        cout << "Esbirro " << i + 1 << ": " << endl;
-        cout << "Vida: " << deq_esbirros->at(i).get_vida() << endl;
-        cout << "Ataque: " << deq_esbirros->at(i).get_ataque() << endl;
-        cout << "Cano: " << (deq_esbirros->at(i).is_cano() ? "Sí" : "No") << endl;
-    }
 }
 
 int main() {
-    
+
+    int daño_realizado = 0, contador = 0, esbirros_derrotados = 0;
+    Entidad mechon;
     deque<Esbirro> deq_esbirros;
     string nombre_archivo = "Ejemplo.txt"; // Luego se puede poner un cin
 
-    get_input(&nombre_archivo, &deq_esbirros);
+    get_input(&nombre_archivo, &mechon, &deq_esbirros);
+
+    // Simulación de ataques
+    while (!deq_esbirros.empty()) {
+
+        // Si el mechón no tiene vida, se termina el juego
+        if (mechon.get_vida() <= 0) {
+            cout << daño_realizado << endl << "RIP mechón" << endl;
+            return 0;
+        }
+
+        // Si el mechón derrota a 5 esbirros, aumenta su ataque
+        if (esbirros_derrotados == 5) {
+            mechon.set_ataque(mechon.get_ataque() + 1);
+            esbirros_derrotados = 0;
+        }
+        
+        // El mechón ataca al esbirro
+        deq_esbirros[0].set_vida(deq_esbirros[0].get_vida() - mechon.get_ataque());
+        daño_realizado += mechon.get_ataque();
+        if (deq_esbirros.front().get_cano()) {
+            contador++;
+        }
+        
+        // El esbirro ataca al mechón
+        mechon.set_vida(mechon.get_vida() - deq_esbirros[0].get_ataque());
+        
+        // Si el esbirro es CANO, se divide luego de la segunda vez que recibe daño
+        if (deq_esbirros[0].get_cano() && deq_esbirros[0].get_vida() > 1 && contador == 2) {
+            Esbirro division(deq_esbirros.front().get_vida() - 1, deq_esbirros.front().get_ataque() - 1, false);
+            deq_esbirros.pop_front();
+            deq_esbirros.push_front(division);
+            deq_esbirros.push_front(division);
+            contador = 0;
+            continue;
+        }
+
+        // Si el esbirro es derrotado, se elimina
+        if (deq_esbirros[0].get_vida() <= 0) {
+            daño_realizado += deq_esbirros[0].get_vida();
+            deq_esbirros.pop_front();
+            esbirros_derrotados++;
+        }
+    }
+    
+    // Si el mechón sobrevive
+    if (mechon.get_vida() > 0) {
+        cout << daño_realizado << endl << "EZ pizi" << endl;
+    } 
+
     return 0;
 }
